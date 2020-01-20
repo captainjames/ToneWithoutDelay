d763d64c63bb0c269b9f7330bf6c5788700af682
captainjames <superdoraemon3@outlook.com>
2020-01-21 00:20:50 +0800

    This is the first version

---
+++ multitone_templete_code/multitone_templete_code.ino
@@ -0,0 +1,88 @@
+/*
+ * THIS IS THE CODE TEMPLETE
+ * Code done by James Wang(Lekkon7193) and thanks for everyone's help.
+ * Inspired by "Blink without Delay".
+ * Use Micros() instead of tone and delay to drive square wave oscillation.
+ * Tested by Arduino Uno.(Runs on SPEEEduino Board).
+ * Pin7 and Pin8 are outputting two diffrent melodies
+ * The LED indicates when the board is playing melodies.
+ */
+
+#include "note_period.h"
+#include "melody8.h"
+#include "melody7.h"
+#include "note_length.h"
+#include "note_length7.h"
+const int buzzer_1_Pin =  7; //BUZZER 1 PIN
+const int buzzer_2_Pin =  8; //BUZZER 2 PIN
+const int ledPin =  13;
+int buzzer_1_State = LOW;
+int buzzer_2_State = LOW;    
+unsigned long previoustime1 = 0; 
+unsigned long previoustime2 = 0;
+unsigned long previoustime3 = 0;
+unsigned long previoustime4 = 0;
+unsigned int counter1 = 0 ;
+unsigned int counter2 = 0 ;
+const int totalnotes_01 = 100; 
+// INDICATE THE NUMBER OF NOTES YOU WANT TO PLAYED ON BUZZER 1. EXCEED THIS NUMBER MEAN ROLL OVER AGAIN.
+const int totalnotes_02 = 100; 
+// INDICATE THE NUMBER OF NOTES YOU WANT TO PLAYED ON BUZZER 2. EXCEED THIS NUMBER MEAN ROLL OVER AGAIN.
+void setup() {
+  pinMode(buzzer_1_Pin, OUTPUT);
+  pinMode(buzzer_2_Pin, OUTPUT);
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);
+  
+}
+void loop() {
+  unsigned long currenttime = micros();
+  unsigned long currentwait = millis();
+  int notepin8= melody8[counter1];
+  int notepin7= melody7[counter2];
+  int notelen8= note8length[counter1];
+  int notelen7= notelength7[counter2];
+  long interval_01 = notepin8; //DETERMIN NOTE 1       
+  long interval_02 = notepin7;  //DETERMIN NOTE 2  
+  if ((currenttime - previoustime1 >= interval_01) and interval_01 != 0) {
+    previoustime1 = currenttime;
+    if (buzzer_1_State == LOW) {
+      buzzer_1_State = HIGH;
+    } else {
+      buzzer_1_State = LOW;
+    }
+    digitalWrite(buzzer_1_Pin, buzzer_1_State);
+    }
+  if ((currenttime - previoustime2 >= interval_02) and interval_02 != 0) {
+    previoustime2 = currenttime;
+    if (buzzer_2_State == LOW) {
+      buzzer_2_State = HIGH;
+    } else {
+      buzzer_2_State = LOW;
+    }
+    digitalWrite(buzzer_2_Pin, buzzer_2_State);
+    }
+  if (currentwait - previoustime3 >= notelen8){
+    previoustime3 = currentwait;
+    Serial.println(interval_01);
+    Serial.println(counter1);
+    digitalWrite(buzzer_1_Pin,LOW);
+    if (counter1 <= totalnotes_01){
+    counter1 = counter1+1;digitalWrite(buzzer_1_Pin,LOW);
+    digitalWrite(ledPin, HIGH);
+    }
+    else{counter1 = 0;digitalWrite(buzzer_1_Pin,LOW);digitalWrite(ledPin, LOW);}
+    }
+
+   if (currentwait - previoustime4 >= notelen7){
+    previoustime4 = currentwait;
+    Serial.println(interval_02);
+    Serial.println(counter2);
+    digitalWrite(buzzer_2_Pin,LOW);
+    if (counter2 < totalnotes_02){
+    counter2 = counter2+1;
+    digitalWrite(buzzer_2_Pin,LOW);
+    }else{counter2 = 0;digitalWrite(buzzer_2_Pin,LOW);}
+    }
+  
+}
---
+++ multitone_templete_code/note_length.h
@@ -0,0 +1,16 @@
+/*This is the duration to be played. Units are in milliseconds.
+ * You need to set the correct timing for each note.
+ * For example, for BPM = 120,
+ * A Whole Note/Rest taking 4 number of beat using value of 2000.
+ * A Half Note/Rest taking 2 number of beat using value of 1000.
+ * A Quarter Note/Rest taking 1 number of beat using value of 500.
+ * An Eighth Note/Rest taking 0.5 number of beat using value of 250.
+ * A Sixteenth Note/Rest taking 0.25 number of beat using value of 125.
+ * A dotted Eighth Note/Rest taking 0.75 number of beat using value of 375.
+ * A dotted Quarter Note/Rest taking 1.5 number of beat using value of 750.
+ * A dotted Half Note/Rest taking 2.5 number of beat using value of 1500.
+ * THIS SEQUENCE IS FOR BUZZER 1.
+ */
+int note8length[]={ 
+
+  };
